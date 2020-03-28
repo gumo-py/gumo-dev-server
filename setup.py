88ac7309110efd38502ba7aeb2b1d549aea55e02
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-03-28 14:07:38 +0900

    use datastore-viewer v0.2.0b0 and Bump v0.1.0a0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 
 name = 'gumo-dev-server'
-version = '0.0.4'
+version = '0.1.0a0'
 description = 'Gumo Dev Server Utilities'
 dependencies = [
     'pyyaml >= 5.1',
     'injector >= 0.13.1',
     'gumo-core >= 0.1.0',
     'gumo-task-emulator >= 0.1.1',
-    'datastore-viewer >= 0.1.0'
+    'datastore-viewer >= 0.2.0b0'
 ]
 
 with open("README.md", "r") as fh:
